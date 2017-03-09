0f3972713c3698880a6ddccbd355163cef1101ee
Gavenchi <johnjoshuaferrer@gmail.com>
2017-03-09 16:02:01 +0800

    Added voter's turnout link

--- resources/views/layouts/shs.blade.php
+++ resources/views/layouts/shs.blade.php
@@ -42,6 +42,9 @@
                 <li class="nav-item">
                   <a class="nav-link" href="/anonresult">Anonymous Results</a>
                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/turnout">Voter's Turnout</a>
+                </li>
                 @endif
                 <li class="nav-item">
 
